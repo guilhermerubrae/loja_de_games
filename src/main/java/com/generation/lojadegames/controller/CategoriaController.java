f0340a5a3407af50ebdc422bee101618cb6c3685
guilhermerubrae <guilhermerubrae@gmail.com>
2024-01-17 19:45:47 -0300

    final

--- src/main/java/com/generation/lojadegames/controller/CategoriaController.java
+++ src/main/java/com/generation/lojadegames/controller/CategoriaController.java
@@ -1,5 +1,32 @@
 package com.generation.lojadegames.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.lojadegames.model.Categoria;
+import com.generation.lojadegames.repository.CategoriaRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/categoria")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 public class CategoriaController {
 
 	@Autowired
@@ -43,5 +70,4 @@ public class CategoriaController {
 
 		categoriaRepository.deleteById(id);
 	}
-
 }

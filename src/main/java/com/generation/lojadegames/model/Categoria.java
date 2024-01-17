013481eb74950eda5a6e210a6a5c83aaf9fdf2fd
guilhermerubrae <guilhermerubrae@gmail.com>
2024-01-17 17:07:35 -0300

    model relacionamento

--- src/main/java/com/generation/lojadegames/model/Categoria.java
+++ src/main/java/com/generation/lojadegames/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.lojadegames.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -23,6 +30,10 @@ public class Categoria {
 	@NotBlank(message = "O atributo texto é Obrigatório!")
 	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
 	private String texto;
+	
+	@OneToMany (fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List <Produto> produto;
 
     public Long getId() {
         return this.id;
@@ -47,5 +58,13 @@ public class Categoria {
     public void setTexto(String texto) {
         this.texto = texto;
     }
+    
+    public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
   
 }
--- src/main/java/com/generation/lojadegames/model/Produto.java
+++ src/main/java/com/generation/lojadegames/model/Produto.java
@@ -6,11 +6,13 @@ import java.time.LocalDateTime;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -41,6 +43,10 @@ public class Produto {
 
 	@UpdateTimestamp
 	private LocalDateTime data;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
 
 	public Long getId() {
 		return id;
@@ -80,6 +86,14 @@ public class Produto {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 
 }
